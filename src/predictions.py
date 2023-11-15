5048fcb78b211bac272328f945c381c50df82c22
Joao <joao.sarraceni@gmail.com>
2023-11-15 14:16:43 -0300

    hotfix: Change paths and load data.

--- src/predictions.py
+++ src/predictions.py
@@ -1,18 +1,21 @@
 import pandas as pd
 from model import Model
+from tqdm import tqdm 
+import os, sys
+tqdm.pandas()
 
 class Predictions:
     def __init__(self, IN_PATH, OUT_PATH):
-        self.data = data
+        self.data = pd.read_csv(IN_PATH)
         self.OUT_PATH = OUT_PATH
         self.model = Model()
         
     def predictions(self):
-        predictions = self.data.input.apply(lambda input: self.model(input))
+        predictions = self.data.input.progress_apply(lambda input: self.model.predict(input))
         self.data['y_pred'] = predictions
         
     def save(self):
-        self.data.to_csv(self.OUT_PATH)
+        self.data.to_csv(self.OUT_PATH, index=False)
         
     def pipeline(self):
         self.predictions()
@@ -24,3 +27,7 @@ def main():
     
     pred = Predictions(train_input, train_output)
     pred.pipeline()
+    
+    
+if __name__ == '__main__':
+    main()
