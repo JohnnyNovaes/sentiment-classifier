bd138676874f0b3780016b1811a6055eba5b5f99
Joao <joao.sarraceni@gmail.com>
2023-11-16 21:43:25 -0300

    feat: Model class, votingclassifier with lg & knn.

--- src/model.py
+++ src/model.py
@@ -1,11 +1,19 @@
-from transformers import pipeline
+from joblib import dump, load
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import VotingClassifier
 
 class Model:
-    def __init__(self, model_name: str = "lxyuan/distilbert-base-multilingual-cased-sentiments-student") -> None:
-        self.model = pipeline(model=model_name)
-    
-    def predict(self, to_predict: str) -> str:
-        return(self.model(to_predict))
-        
+    def __init__(self):
+        clf1 = LogisticRegression(multi_class='multinomial', random_state=42, class_weight='balanced')
+        clf2 = KNeighborsClassifier(n_neighbors=3)
+        self.classifier = VotingClassifier(estimators=[('lr',  clf1),
+                                                       ('knn', clf2)], voting='soft')
+    @staticmethod
+    def load_model(PATH):
+        self.classifier = load(PATH)    
+                
+    def save_model(self, PATH):
+        dump(self.classifier, PATH)
         
         
